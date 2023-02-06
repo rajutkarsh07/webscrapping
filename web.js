4ee4baab47c46f336f86866f0554f12e802607c3
utkarsh_raj <rajutkarsh2505@gmail.com>
2023-02-06 23:09:29 +0530

    used axios instead of request

--- web.js
+++ web.js
@@ -1,31 +1,62 @@
 const request = require("request");
 const cheerio = require("cheerio");
+const axios = require("axios");
+const express = require("express");
 
-request("https://utkarsh-raj.netlify.app/", (err, res, html) => {
-  if (!err && res.statusCode == 200) {
-    // console.log(html);
+// request("https://utkarsh-raj.netlify.app/", (err, res, html) => {
+//   if (!err && res.statusCode == 200) {
+//     // console.log(html);
 
-    const $ = cheerio.load(html);
+//     const $ = cheerio.load(html);
 
-    const about = $(".header-nav");
-    console.log(about.html());
-    // console.log(about.text());
+//     const about = $(".header-nav");
+//     console.log(about.html());
+//     // console.log(about.text());
 
-    console.log("---------------- new line -----------------");
+//     console.log("---------------- new line -----------------");
 
-    let arr = [];
+//     let arr = [];
 
-    about.each((i, e) => {
-      const item = $(e).text();
-      arr.push(item);
-      // console.log(item);
+//     about.each((i, e) => {
+//       const item = $(e).text();
+//       arr.push(item);
+//       // console.log(item);
+//     });
+
+//     arr.toString();
+
+//     console.log(arr);
+//     console.log(arr.length);
+
+//     console.log("---------------- new line -----------------");
+//   }
+// });
+
+const PORT = 4001;
+
+const app = express();
+
+axios("https://www.youtube.com/").then((err, res, html) => {
+  if (err) {
+    console.log(err);
+  }
+  if (!err) {
+    // console.log(res);
+    const htmlData = res.data;
+    const $ = cheerio.load(htmlData);
+
+    const arr = [];
+
+    $(".style-scope").each((ind, ele) => {
+      const title = $(ele).children(".yt-simple-endpoint").text();
+      const titleUrl = $(ele).children(".yt-simple-endpoint").attr("href");
+      arr.push({ title, titleUrl });
     });
 
-    arr.toString();
+    console.log("-------------------------");
 
     console.log(arr);
-    console.log(arr.length);
-
-    console.log("---------------- new line -----------------");
   }
 });
+
+app.listen(PORT, () => console.log("server started at port 4001"));
